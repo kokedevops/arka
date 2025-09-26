241fa2083d334d36f6dbecbcad41c8b16d929dfe
Jorge Koke Valenzuela <jorge.valenzuela.diaz@ciisa.cl>
2025-09-26 02:17:53 -0300

    error compilacion
    Signed-off-by: Jorge Koke Valenzuela <jorge.valenzuela.diaz@ciisa.cl>

--- arka-security-common/src/main/java/com/arka/security/config/ArkaSecurityAutoConfiguration.java
+++ arka-security-common/src/main/java/com/arka/security/config/ArkaSecurityAutoConfiguration.java
@@ -3,7 +3,7 @@ package com.arka.security.config;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
+import org.springframework.data.jdbc.repository.config.EnableJdbcRepositories;
 
 /**
  * Configuración de auto-configuración para el módulo de seguridad ARKA
@@ -14,7 +14,7 @@ import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
     "com.arka.security.config"
 })
 @EntityScan(basePackages = "com.arka.security.domain.model")
-@EnableR2dbcRepositories(basePackages = "com.arka.security.domain.repository")
+@EnableJdbcRepositories(basePackages = "com.arka.security.domain.repository")
 public class ArkaSecurityAutoConfiguration {
     
 }
